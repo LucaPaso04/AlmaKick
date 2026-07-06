29584cfa8717e233be08674cd4068105dda85787
LucaPaso04 <lucapasini0404@gmail.com>
2026-07-06 18:05:19 +0200

    add favicon

--- views/layout.php
+++ views/layout.php
@@ -40,6 +40,8 @@ if ($userAvatar) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="AlmaKick - Organizza e trova partite di calcetto nella tua zona">
     <title><?= e($title ?? 'AlmaKick') ?></title>
+    <!-- Favicon -->
+    <link rel="icon" type="image/svg+xml" href="<?= url('/images/logo.svg') ?>">
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
